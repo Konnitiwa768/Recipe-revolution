c3eae6993be070a596fd68f42a2ef60f087b6988
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-05-03 13:02:15 +0900

    Create RecipeRevolution.java

---
+++ src/main/java/com/sakalti/reciperevolution/RecipeRevolution.java
@@ -0,0 +1,10 @@
+package com.example.reciperevolution;
+
+import net.fabricmc.api.ModInitializer;
+
+public class RecipeRevolution implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        // レシピしか追加しないので、特に何もしない
+    }
+}
